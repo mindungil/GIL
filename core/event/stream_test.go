60f1d5bce77db1349f08a70698c807a080d0b698
mindungil <alswnsrlf12@naver.com>
2026-04-26 10:32:23 +0900

    test(core/event): add Len() coverage test

--- core/event/stream_test.go
+++ core/event/stream_test.go
@@ -25,3 +25,16 @@ func TestStream_Append_DuplicateIDFails(t *testing.T) {
 	_, err := s.Append(Event{ID: 5, Type: "test"})
 	require.Error(t, err)
 }
+
+func TestStream_Len(t *testing.T) {
+	s := NewStream()
+	require.Equal(t, 0, s.Len())
+
+	_, err := s.Append(Event{Type: "test"})
+	require.NoError(t, err)
+	require.Equal(t, 1, s.Len())
+
+	_, err = s.Append(Event{Type: "test"})
+	require.NoError(t, err)
+	require.Equal(t, 2, s.Len())
+}
